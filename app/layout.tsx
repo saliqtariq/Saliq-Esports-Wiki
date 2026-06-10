7e70847edfcbd9e4f9d57e0ee5305578ec169f4b
saliqtariq <saliqtariq2@gmail.com>
2026-06-10 20:49:27 +0500

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata, Viewport } from "next";
 import { Syne, DM_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID || "G-CCNZWSX58J";
 
 const syne = Syne({
   subsets: ["latin"],
@@ -42,6 +45,18 @@ export default function RootLayout({
       lang="en"
       className={`${syne.variable} ${dmSans.variable} h-full w-full overflow-x-hidden antialiased`}
     >
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <body className="min-h-full w-full flex flex-col font-body overflow-x-hidden">{children}</body>
     </html>
   );
